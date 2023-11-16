23993d5f74cf8a50f6fa4eae73d469327912659b
Breno Brito <breno.silvabrito@hotmail.com>
2023-11-16 18:18:11 +0000

    Finish task B

--- DataExpCode.r
+++ DataExpCode.r
@@ -7,11 +7,11 @@ dataframe <- read.csv("./CriminalDataset.csv")
 
 # Task A
 
-# Categorical variables (Statistic, Statisc Label, Garda Station, Type of Offence and Unit)
+# Categorical variables (Statistic, Statistic Label, Garda Station, Type of Offence and Unit)
 # Discrete variables (TList, Year, Station Code, Identifier and Value)
 # Continuous variables ( )
 
-# Create a bar chart for categorical variables visualization (Visualize the total occurrences for each type of offence)
+# Create a bar chart to Visualize the total occurrences for each type of offence
 
 # Aggregate the VALUE variable by summing it up for each Identifier
 aggregated_data <- dataframe %>%
@@ -27,4 +27,34 @@ ggplot(aggregated_data, aes(x = reorder(Type.of.Offence, Total_Value), y = Total
   theme_minimal() +
   theme(axis.text.x = element_text(angle = 90, hjust = 1)) +
   scale_y_continuous(labels = scales::comma)
+
+# Check for missing values in each column 
+colSums(is.na(dataframe))
+
+# Task B
+
+# Select numerical columns
+numerical_columns <- subset(dataframe, select = sapply(dataframe, is.numeric))
+
+# Calculate mean for each numerical column
+mean_values <- sapply(numerical_columns, function(x) mean(x, na.rm = TRUE))
+mean_values
+
+# Calculate median for each numerical column
+median_values <- sapply(numerical_columns, function(x) median(x, na.rm = TRUE))
+median_values
+
+# Calculate minimum for each numerical column
+min_values <- sapply(numerical_columns, function(x) min(x, na.rm = TRUE))
+min_values
+
+# Calculate maximum for each numerical column
+max_values <- sapply(numerical_columns, function(x) max(x, na.rm = TRUE))
+max_values
+
+# Calculate standard deviation for each numerical column
+sd_values <- sapply(numerical_columns, function(x) sd(x, na.rm = TRUE))
+sd_values
+
+
 
